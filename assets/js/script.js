414d9818e7a585939c3fe5d6b60e79cad57da0e1
Marcelo Henrique <mah_henrique4478@hotmail.com>
2024-05-27 12:32:08 -0300

    Logica JS - Contato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,62 @@
+let sobre = document.querySelector("#sobre");
+
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+
+let nomeOk = false;
+let emailOk = false;
+let messageOk = false;
+let cepOk = false;
+
+function validarNome() {
+  let txtNome = document.querySelector("#txtNome");
+
+  if (nome.value.length < 3) {
+    txtNome.innerHTML = "Nome muito curto";
+    txtNome.style.color = "red";
+    nomeOk = false;
+  } else {
+    txtNome.innerHTML = "";
+    nomeOk = true;
+  }
+}
+
+function validarEmail() {
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let txtEmail = document.querySelector("#txtEmail");
+
+  if (email.value.match(regex)) {
+    txtEmail.innerHTML = "";
+    emailOk = true;
+  } else {
+    txtEmail.innerHTML = "E-mail inválido";
+    txtEmail.style.color = "red";
+    emailOk = false;
+  }
+}
+
+function validarMessage() {
+  let txtMensagem = document.querySelector("#txtMensagem");
+
+  if (message.value.length >= 50) {
+    txtMensagem.innerHTML = "Mensagem muito grande!";
+    txtMensagem.style.color = "red";
+    messageOk = false;
+  } else {
+    txtMensagem.innerHTML = "";
+    messageOk = true;
+  }
+}
+
+function enviarForm() {
+  if (nomeOk === true && emailOk === true && messageOk === true) {
+    alert(nome.value + ", Obrigado pelo contato, aguarde nosso retorno.");
+  } else {
+    alert("Por favor, preencha todos os campos corretamente.");
+  }
+}
+
 async function getApiGithub() {
 
     try {
